2547592235e91fe3de4b456fdac3c4256b9d101d
Atharva <atharvakutwal2002@gmail.com>
2022-09-21 22:25:09 +0530

    cartController

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,23 +1,6 @@
 const Cart = require("./../models/cartModel");
 const Product= require('./../models/productModel');
 
-// exports.populateCart = async (req, res) => {
-//   try {
-//     const cartContainer = await Cart.create(req.body);
-//     res.status(201).json(cartContainer);
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// };
-
-// exports.getCartItems = async (req, res) => {
-//   try {
-//     const cartItems = await Cart.findById();
-//     res.status(201).json(cartItems);
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// };
 
 exports.getCartItems= async (req, res) => {
     const {id: userId} = req.params;
